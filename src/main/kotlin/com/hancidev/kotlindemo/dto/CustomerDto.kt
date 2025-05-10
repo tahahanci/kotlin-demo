2a0ec09e32fd57893396c7455371369827772241
Taha Hancı <taha.hanci@std.bogazici.edu.tr>
2025-05-10 14:56:10 +0300

    Add age validation for customers and update CustomerDto
    Implemented age validation to ensure customers are at least 18 years old. Updated `CustomerDto` to include a `birthDate` field and refactored logic for email-based customer existence checks. This ensures better data integrity and validation during customer creation.

--- src/main/kotlin/com/hancidev/kotlindemo/dto/CustomerDto.kt
+++ src/main/kotlin/com/hancidev/kotlindemo/dto/CustomerDto.kt
@@ -1,3 +1,3 @@
 package com.hancidev.kotlindemo.dto
 
-data class CustomerDto(val name: String, val email: String)
+data class CustomerDto(val name: String, val email: String, val birthDate: String)
--- src/main/kotlin/com/hancidev/kotlindemo/service/CustomerService.kt
+++ src/main/kotlin/com/hancidev/kotlindemo/service/CustomerService.kt
@@ -5,6 +5,9 @@ import com.hancidev.kotlindemo.entity.Customer
 import com.hancidev.kotlindemo.repository.CustomerRepository
 import com.hancidev.kotlindemo.service.mapper.CustomerMapper
 import org.springframework.stereotype.Service
+import java.time.LocalDate
+import java.time.Period
+import java.time.format.DateTimeFormatter
 
 @Service
 class CustomerService(private val customerRepository: CustomerRepository,
@@ -15,11 +18,23 @@ class CustomerService(private val customerRepository: CustomerRepository,
     }
 
     fun saveCustomer(customerDto: CustomerDto): Customer? {
-        customerRepository.findCustomerByEmail(customerDto.email)
-            ?.let { throw IllegalArgumentException() }
+        val birthDate = LocalDate.parse(customerDto.birthDate, DATE_FORMATTER)
+        val age = Period.between(birthDate, LocalDate.now()).years
 
-        return customerMapper.customerFromCustomerDto(customerDto)
-            .let(customerRepository::save)
+        if (age < 18) {
+            throw IllegalArgumentException("Customer must be at least 18!")
+        }
+
+        if (isCustomerExist(customerDto.email)) {
+            throw IllegalArgumentException("Customer exists with given email!")
+        }
+
+        val customer = customerMapper.customerFromCustomerDto(customerDto)
+        return customerRepository.save(customer)
+    }
+
+    companion object {
+        private val DATE_FORMATTER: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
     }
 
     fun deleteCustomer(email: String) {
@@ -29,6 +44,10 @@ class CustomerService(private val customerRepository: CustomerRepository,
 
     fun getAllCustomers() : List<Customer> {
         return customerRepository.findAllByEmailIsNotNull()
+    }
+
+    private fun isCustomerExist(email: String) : Boolean {
+        return customerRepository.findCustomerByEmail(email) != null
     }
 
 }
